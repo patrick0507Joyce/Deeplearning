a470950e96f12c668001962888da92d079ad383d
patrick0609 <sxh20150609@gmail.com>
2018-07-28 23:30:39 +0100

    Classification for crowd analysis - create 3 main functions for the data pretreatment - add the ROC and AUC curve for the project - merge  and classify scenes together

--- Crowd_read.py
+++ Crowd_read.py
@@ -3,7 +3,7 @@ from PIL import Image
 import glob
 import scipy.io
 
-# grass 1,2 followed by square 1, 2, 3 and inside 1, 2, 3(problem), 4, 5, 6
+# grass 1,2 followed by  inside 1, 2, 3(problem), 4, 5, 6 square 1, 2, 3
 
 
 matlist = glob.glob("crowd-dataset/1MeiYiZhenEntropy0*.mat")
@@ -11,41 +11,50 @@ matlist = glob.glob("crowd-dataset/1MeiYiZhenEntropy0*.mat")
 mat_set_01 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy01.mat")
 feature01 = mat_set_01['C']
 print(feature01.shape)
-mat_set_02 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy02.mat")
+mat_set_02 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy010.mat")
 feature02 = mat_set_02['C']
 print(feature02.shape)
-mat_set_03 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy03.mat")
+mat_set_03 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy011.mat")
 feature03 = mat_set_03['C']
 print(feature03.shape)
-mat_set_04 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy04.mat")
+mat_set_04 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy06.mat")
 feature04 = mat_set_04['C']
 print(feature04.shape)
 mat_set_05 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy05.mat")
 feature05 = mat_set_05['C']
 print(feature05.shape)
-mat_set_06 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy06.mat")
+mat_set_06 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy04.mat")
 feature06 = mat_set_06['C']
 
 print(feature06.shape)
-mat_set_07 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy07.mat")
+mat_set_07 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy03.mat")
 feature07 = mat_set_07['C']
 print(feature07.shape)
-mat_set_08 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy08.mat")
+mat_set_08 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy02.mat")
 feature08 = mat_set_08['C']
 print(feature08.shape)
-mat_set_09 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy09.mat")
+mat_set_09 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy07.mat")
 feature09 = mat_set_09['C']
 print(feature09.shape)
-mat_set_010 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy010.mat")
+mat_set_010 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy08.mat")
 feature010 = mat_set_010['C']
 print(feature010.shape)
-mat_set_011 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy011.mat")
+mat_set_011 = scipy.io.loadmat("crowd-dataset/1MeiYiZhenEntropy09.mat")
 feature011 = mat_set_011['C']
 print(feature011.shape)
 
-print(feature01.shape[1] + feature02.shape[1] + feature03.shape[1] +
-      feature04.shape[1] + feature05.shape[1] + feature06.shape[1] +
-      feature07.shape[1] + feature08.shape[1] + feature09.shape[1] +
-      feature010.shape[1] + feature011.shape[1])
-
-print(feature02[0, 600:690])
+feature = []
+feature.append(0)
+feature.append(feature01)
+feature.append(feature02)
+feature.append(feature03)
+feature.append(feature04)
+feature.append(feature05)
+feature.append(feature06)
+feature.append(feature07)
+feature.append(feature08)
+feature.append(feature09)
+feature.append(feature010)
+feature.append(feature011)
+feature_smoothed = feature
+feature_length = 0
---
+++ New_Crowd_Read.py
@@ -0,0 +1,51 @@
+import numpy as np
+from PIL import Image
+import glob
+import scipy.io
+
+# grass 1,2 followed by  inside 1, 2, 3(problem), 4, 5, 6 square 1, 2, 3
+
+
+matlist = glob.glob("new-crowd-dataset/1MeiYiZhenEntropy0*.mat")
+# mat_set = np.array([np.array(scipy.io.loadmat(matname)) for matname in matlist])
+mat_set_01 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy01.mat")
+feature01 = mat_set_01['C']
+print(feature01.shape)
+mat_set_02 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy02.mat")
+feature02 = mat_set_02['C']
+print(feature02.shape)
+mat_set_03 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy03.mat")
+feature03 = mat_set_03['C']
+print(feature03.shape)
+mat_set_04 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy04.mat")
+feature04 = mat_set_04['C']
+print(feature04.shape)
+mat_set_05 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy05.mat")
+feature05 = mat_set_05['C']
+print(feature05.shape)
+mat_set_06 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy06.mat")
+feature06 = mat_set_06['C']
+
+print(feature06.shape)
+mat_set_07 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy07.mat")
+feature07 = mat_set_07['C']
+print(feature07.shape)
+mat_set_08 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy08.mat")
+feature08 = mat_set_08['C']
+print(feature08.shape)
+mat_set_09 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy09.mat")
+feature09 = mat_set_09['C']
+print(feature09.shape)
+mat_set_010 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy010.mat")
+feature010 = mat_set_010['C']
+print(feature010.shape)
+mat_set_011 = scipy.io.loadmat("new-crowd-dataset/1MeiYiZhenEntropy011.mat")
+feature011 = mat_set_011['C']
+print(feature011.shape)
+
+print(feature01.shape[1] + feature02.shape[1] + feature03.shape[1] +
+      feature04.shape[1] + feature05.shape[1] + feature06.shape[1] +
+      feature07.shape[1] + feature08.shape[1] + feature09.shape[1] +
+      feature010.shape[1] + feature011.shape[1])
+
+
